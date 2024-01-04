ab5c16272038d82772df64cf064123e04a9d2f47
Ricardo Lemos <rycardokk@gmail.com>
2024-01-04 18:42:14 -0300

    feat: criacao do arquivo

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,17 @@
+    $(document).ready(function () {
+    $('form').submit(function (event) {
+        var name = $('#name').val();
+        var price = $('#price').val();
+        var description = $('#description').val();
+
+        if (name.trim() === '' || price.trim() === '' || description.trim() === '') {
+            alert('Por favor, preencha todos os campos.');
+            event.preventDefault(); // Impedir o envio do formulário
+        }
+
+        if (isNaN(price)) {
+            alert('Por favor, insira um valor válido para o preço.');
+            event.preventDefault(); // Impedir o envio do formulário
+        }
+    });
+});
